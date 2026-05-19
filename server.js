5637db586fffd6f0d06119f1640e9bed42ebe5bb
krysnaaw <kresnawicaksono793@gmail.com>
2026-05-19 13:21:26 +0700

    update log

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 // ==========================
 // MYSQL CONNECTION
