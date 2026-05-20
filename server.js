c98feb3221a3056b9a63bb28c514bc0cc9c9805e
krysnaaw <kresnawicaksono793@gmail.com>
2026-05-20 13:28:44 +0700

    fix railway port

--- server.js
+++ server.js
@@ -192,6 +192,10 @@ app.post("/payment", async (req, res) => {
 // ==========================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server jalan di port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Backend aktif 🚀");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server jalan di port ${PORT}`);
 });
